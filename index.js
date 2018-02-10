6b8a0c2aa1c9ae5b6fb33817f4be50ea6f54f683
Amakawa <firesidecake@gmail.com>
2018-02-10 16:03:10 +1100

    Chatbot

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-  socket.on('chat message', (msg) => {
-    console.log('Message ', msg);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
-
-
-// const express = require('express');
-// // import express from 'express'
-// const app = express();
-//
-//
-//
-// app.get('/', (req, res) => "Hello, world!")
-// app.get('/trung', (req, res) => res.send('Is the best!'))
-//
-// app.get('/get_response', (req, res) => {
-//     // Do something
-//
-//     res.json({
-//         name: 'trung',
-//     })
-// })
-//
-// app.listen(3012, () => console.log('Example app listening on port 3000!'))
