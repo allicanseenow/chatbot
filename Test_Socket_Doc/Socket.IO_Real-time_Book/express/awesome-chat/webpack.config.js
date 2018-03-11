350bb5a0d07f2cbeff31de8b8e5c099875ced919
Amakawa <firesidecake@gmail.com>
2018-03-12 03:09:23 +1100

    2 days (40 hours) of setting up webpacks

---
+++ Test_Socket_Doc/Socket.IO_Real-time_Book/express/awesome-chat/webpack.config.js
@@ -0,0 +1,102 @@
+const webpack = require('webpack');
+const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanwebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+const transpiledPath = path.resolve(__dirname, 'public', 'transpiled_javascripts');
+console.log('transpi;e path', transpiledPath)
+
+console.log('Fuck---------------         --------------              ----------------------           -------------   -----')
+
+module.exports = {
+  entry: {
+    rooms: './public/javascripts/rooms.js',
+    chat: './public/javascripts/chat.js'
+  },
+  resolve: {
+    // you can now require('file') instead of require('file.js')
+    extensions: ['.js', '.json']
+  },
+  output: {
+    // chunkFilename: '[name].bundle.js',
+    filename: '[name].js',
+    path: transpiledPath
+  },
+  devServer: {
+    contentBase: './public/transpiled_javascripts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' }
+        ]
+      },
+      // {
+      //   test: /\.scss$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     // scss-loader after postcss-loader
+      //     use: ['css-loader', 'postcss-loader', 'scss-loader']
+      //   })
+      // },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanwebpackPlugin([transpiledPath]),
+    // new ExtractTextPlugin('styles.css')
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: true,
+        },
+        ie8: false,
+        output: {
+          comments: false,
+          beautify: false,
+        },
+        toplevel: true,
+      },
+      sourceMap: true,
+    }),
+    // new BrowserSyncPlugin({
+    //   host: 'localhost',
+    //   port: 3000,
+    //   proxy: 'http://localhost:3100/'
+    // }, {
+    //   reload: false,
+    // })
+  ],
+  // target: 'web',
+  stats: {
+    colors: true
+  },
+  devtool: 'inline-source-map',
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
+  watch: true
+};
